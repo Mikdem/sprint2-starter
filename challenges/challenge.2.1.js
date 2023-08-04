3ac115ea2ad92d4bf516cf78d6467d43208e8ce7
Mikdem <76061628+Mikdem@users.noreply.github.com>
2023-08-04 12:47:48 +0300

    updating challenges

---
+++ challenges/challenge.2.1.js
@@ -0,0 +1,176 @@
+/* eslint-disable no-unused-vars */
+import { Job, Candidate, Skill } from '../common/model.js';
+
+/**
+ * Part 1: Basic utility functions.
+ *
+ * The challenge is to create optimal solutions for implementing some of the
+ * common utility functions that we will need to build our ATS solution.
+ *
+ *
+ */
+
+/**
+ * Filter the given jobs list and return only jobs that start between the given
+ * start and end dates.
+ *
+ * @param {Array<Job>} jobs
+ * @param {Date} startDate
+ * @param {Date} endDate
+ */
+const filterByDate = (jobs, startDate, endDate) => {
+  // ----- Challenge 2.1.1 - Complete the function here ---- //
+
+    // Convert the start and end date strings to Date objects
+    const startDateTime = new Date(startDate);
+    const endDateTime = new Date(endDate);
+
+    const filteredJobs = jobs.filter(job => {
+      // Convert the job's start date string to a Date object
+      const jobStartDate = new Date(job.startDate);
+      
+      // Check if the job's start date is within the range of the given start and end dates
+      return jobStartDate >= startDateTime && jobStartDate <= endDateTime;
+    });
+  
+    return filteredJobs;
+
+  
+};
+
+/**
+ * Filter the given candidates list and return only candidates that are born
+ * on or after the given date.
+ *
+ * @param {Array<Candidate>} candidates
+ * @param {Date} date
+ */
+const filterByBornAfter = (candidates, date) => {
+  // ----- Challenge 2.1.2 - Complete the function here ---- //
+  
+ const filterByBornday = candidates.filter(newCandidate =>{ 
+  
+  const dateOfBirth = new Date(newCandidate.dateOfBirth);
+
+  return date<= dateOfBirth;
+ }
+ )
+
+ return filterByBornday;
+
+};
+
+/**
+ * Sort the given candidate list, so that candidates with most skills,
+ * regardless of the levels, are at the beginning.
+ *
+ * @param {*} candidateList
+ * @returns
+ */
+const orderBySkills = (candidateList) => {
+  // ----- Challenge 2.1.3 - Complete the function here ---- //
+
+  const sortedCandidate = candidateList.sort((a, b) => {
+    if(a.skills.length > b.skills.length){
+      return -1; // if a > b, a comes before b
+    } else if(a.skills.length < b.skills.length){
+      return 1; // if a < b, a comes after b
+    }else
+    return 0; // a = b, their original order is intact.
+  })
+
+  return sortedCandidate;
+};
+
+/**
+ * Sort the given candidate list, so that candidates with more valuable skills,
+ * are at the beginning.
+ * Each skill will be weighed as follows: Expert levels count as 10, Advanced levels count as 5, Beginner levels count as 1
+ *
+ * @param {Array<Candidate>} candidateList
+ * @returns
+ */
+const orderByWeightedSkills = (candidateList) => {
+  // ----- Challenge 2.1.4 - Complete the function here ---- //
+
+  const skillslevel = 0;
+
+  candidateList.forEach ((candidate) => {
+    if(candidate.skills.level === 'Expert'){
+      candidate.skills.level = 10;
+    }else if(candidate.skills.level === 'Advanced'){
+      candidate.skills.level = 5;
+    }else if(candidate.skills.level === 'Beginner'){
+      candidate.skills.level = 1;
+    }
+  });
+ const sortByWeightedSkill = candidateList.sort((a, b) => {
+  if(a.candidateList.skills.level > b.candidateList.skills.level){
+    return -1; // if a > b, a comes before b
+  } else if(a.skillslevel < b.skillslevel){
+    return 1; // if a < b, a comes after b
+  }else
+  return 0; // a = b, their original order is intact.
+})
+
+
+  return sortByWeightedSkill;
+};
+
+/**
+ * Return the ratio of female/male candidates in the list
+ * @param {Array<Candidate>} candidateList
+ * @returns a floating point number indicating the ratio
+ */
+const genderRatio = (candidateList) => {
+
+  // ----- Challenge 2.1.5 - Complete the function here ---- //
+
+  let femaleNumber = 0;
+  let maleNumber = 0;
+
+  for (const candidate of candidateList) {
+    if (candidate.gender === 'F') {
+      femaleNumber++;
+    } else if (candidate.gender === 'M') {
+      maleNumber++;
+    }
+  }
+
+  const ratio = femaleNumber / maleNumber;
+  return ratio;
+
+};
+
+/**
+ * Return the month with the highest number of jobs starting,
+ * indicated by the startDate property of each Job.
+ * @param {Array<Job>} jobs
+ * @returns number (0-11)
+ */
+const busiestMonth = (jobs) => {
+  // ----- Challenge 2.1.6 - Complete the function here ---- //
+
+  let busyMonth = 1;
+  for (const job of jobs) {
+      if(job.startDate.getMonth()){
+        busyMonth++;
+      }
+  }
+
+  return 0;
+};
+
+/**
+ * Return the skill name that is required the most in the given list of Jobs,
+ * indicated by the requiredSkills property of each Job.
+ *
+ * @param {Array<Job>} jobs
+ */
+const mostInDemandSkill = (jobs) => {
+
+  // ----- Challenge 2.1.7 - Complete the function here ---- //
+
+};
+
+export { filterByDate, filterByBornAfter, orderBySkills, orderByWeightedSkills, genderRatio, busiestMonth, mostInDemandSkill };
---
+++ challenges/challenge.2.3.js
@@ -0,0 +1,115 @@
+// eslint-disable-next-line no-unused-vars
+import { Candidate, Job } from '../common/model.js';
+
+/**
+ * Part 3: Job matching
+ * ------------------------------------------
+ *
+ */
+
+/**
+ * This function returns true only if the candidateSkill is a match
+ * for the job skill:
+ * - the names are identical, regardless of case
+ * - the candidate's skill level is higher or equal to the job skill level
+ *
+ *
+ * @param {Skill} candidateSkill
+ * @param {Skill} jobSkill
+ */
+const skillsMatch = (candidateSkill, jobSkill) => {
+  // ----- Challenge 2.3.1 - Complete the function here ---- //
+
+  if(candidateSkill.name.toUpperCase() === jobSkill.name.toUpperCase() && candidateSkill.level >= jobSkill.level){
+    return true;
+  }else
+
+  return false;
+};
+
+/**
+ * Returns true if the candidate meets the gender requirements
+ * of the given job. If the job has no gender requirements, all candidates
+ * are suitable.
+ *
+ * @param {Candidate} candidate
+ * @param {Job} job
+ */
+const suitableGender = (candidate, job) => {
+  // ----- Challenge 2.3.2 - Complete the function here ---- //
+
+  if (candidate.gender === job.requiredGender || job.requiredGender == undefined){
+    return true;
+  }else
+  return false;
+};
+
+/**
+ *
+ * This function returns a number between 0 and 100 indicating the suitability
+ * of the candidate for the given job. Rules are as follows:
+ * - 20% of the score is allocated for gender suitablity.
+ * - 80% is reserved for matching skills. A candidate who has all the skills required by the job will
+ * get 80%.
+ * The result will be rounded to the closest whole number.
+ *
+ * @param {String} name
+ * @returns String
+ */
+const suitabilityScore = (candidate, job) => {
+  // ----- Challenge 2.3.3 - Complete the function here ---- //
+
+  let genderScore = 0;
+  let skillScore = 0;
+  let numberOfSkill = 0;
+  let score = 0;
+
+  if(suitableGender(candidate, job) === true){
+    genderScore +=20;
+  }
+
+  for(let i=0; i<job.requiredSkills.length; i++){
+    for(let j=0; j<candidate.skills.length; j++){
+      if(skillsMatch(candidate.skills[j], job.requiredSkills[i]) === true){
+        numberOfSkill += 1;
+      }
+    };
+  };
+
+  skillScore = Math.round(numberOfSkill/job.requiredSkills.length*80);
+
+  score = genderScore + skillScore;
+
+  return score;
+};
+
+/**
+ * The 'hotness' of a candidate is defined by the number of jobs
+ * for which their suitability score is greater than 80.
+ * This function returns the highest 'hotness' score from the candidates list
+ * and the provided jobs list.
+ *
+ * @param {Array<Candidate>} candidates
+ * @param {Array<Job>} jobs
+ * @returns number
+ */
+const hottestCandidate = (candidates, jobs) => {
+  // ----- Challenge 2.3.4 - Complete the function here ---- //
+
+  let hotness = 0;
+
+  for(let i=0; i<jobs.length; i++){
+    for(let j=0; j<candidates.length; j++){
+      const hot = suitabilityScore(candidates[j], jobs[i]);
+      if(hot > 80){
+        hotness++;
+      }
+    };
+  };
+
+
+
+  return hotness;
+};
+
+export { skillsMatch, suitableGender, suitabilityScore, hottestCandidate };
